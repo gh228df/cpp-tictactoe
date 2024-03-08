7d47158fcde0a48b90d8bee3ee8d0c57aee5088e
gh228df <144490574+gh228df@users.noreply.github.com>
2024-03-08 23:09:02 +0100

    Update ttt.cpp
    Upgraded algorithm, improved execution time by 500%, recycled old code, still can't figure out how to modify algorithm so I don't have to explicitly modify array value at lines 182-193

--- ttt.cpp
+++ ttt.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
-#include <fstream>
+#include <vector>
 #include <bitset>
 #include <chrono>
+#include <algorithm>
 
 using namespace std;
 using namespace chrono;
 
-char posopti[19561][11], winarr[19561] = {0};
-short pr[18913];
-const short power3[2][9] = {{1, 3, 9, 27, 81, 243, 729, 2187, 6561} , {2, 6, 18, 54, 162, 486, 1458, 4374, 13122}};
+unsigned char winarr[19683] = {0};
 
-const bool EXTRACOCKY = 0;
+unsigned short pr[19683][9];
+
+const unsigned short power3[9] = {1, 3, 9, 27, 81, 243, 729, 2187, 6561};
+
+const bool EXTRACOCKY = 1;
+
+bool checkWin(char board[9], char sw) {
+    bitset<9> pl;
+    for (short i = 0; i < 9; ++i)
+    	pl.set(i, board[i] == sw);
+    const bitset<9> win[] = {0b111000000,0b000111000,0b000000111,0b100100100,0b010010010,0b001001001,0b100010001,0b001010100};
+    for (const auto& combination : win)
+		if ((pl & combination) == combination) return true;
+    return false;
+}
 
 void display(short compcurpos)
 {
 	for (short j = 9; j > 0; --j){
 		if (j % 3 == 0 && j != 9) cout << "---+---+---" << endl;
 		for (short u = 0; u < 9; ++u){
-			if ((compcurpos / power3[0][(((j + 2) / 3) * 3 - u / 3 - 1)]) % 3 == (8 - u % 3 + (j - 1) % 3) % 2 + 1)
-				if ((compcurpos / power3[0][(((j + 2) / 3) * 3 - u / 3 - 1)]) % 3 == 1) cout << "\033[1;31m#\033[0m";
+			if ((compcurpos / power3[(((j + 2) / 3) * 3 - u / 3 - 1)]) % 3 == (8 - u % 3 + (j - 1) % 3) % 2 + 1)
+				if ((compcurpos / power3[(((j + 2) / 3) * 3 - u / 3 - 1)]) % 3 == 1) cout << "\033[1;31m#\033[0m";
 				else cout << "\033[1;33m#\033[0m";
-			else if (j % 3 == 2 && u % 3 == 1 && (compcurpos / power3[0][(((j + 2) / 3) * 3 - u / 3 - 1)]) % 3 == 0) cout << ((j + 2) / 3) * 3 - u / 3;
+			else if (j % 3 == 2 && u % 3 == 1 && (compcurpos / power3[(((j + 2) / 3) * 3 - u / 3 - 1)]) % 3 == 0) cout << ((j + 2) / 3) * 3 - u / 3;
 			else cout << " ";
 			if (u % 3 == 2 && u < 8) cout << "|";
 		}
@@ -38,217 +51,191 @@ void input(short *compcurpos, bool mul)
         cin.ignore(numeric_limits<streamsize>::max(), '\n');
         input(compcurpos, mul);
     }else
-		if ((*compcurpos / power3[0][p - 1]) % 3 == 0 && p > 0 && p < 10)
-			*compcurpos += power3[mul][p - 1];
+		if ((*compcurpos / power3[p - 1]) % 3 == 0 && p > 0 && p < 10)
+			*compcurpos += (power3[p - 1] << mul);
 		else
 			input(compcurpos, mul);
 }
 
-short turn(short num, short *compcurpos, bool mul)
-{
-	char sw = posopti[num][9];
-	if(sw == 1) sw = posopti[num][0];
-	else sw = posopti[num][rand() % sw];
-	*compcurpos += power3[mul][sw];
-	return sw;
-}
-
 void draw(bool sw)
 {
-	cout << endl << "\033[1;32m##  ###  #  # # #\033[0m" << endl << "\033[1;32m# # # # # # # # #\033[0m" << endl << "\033[1;32m# # ##  ### # # #\033[0m" << endl << "\033[1;32m# # # # # # # # #\033[0m" << endl << "\033[1;32m##  # # # #  # #\033[0m" << endl << endl;
+	cout << endl << "\033[1;32m##  ###  #  # # #" << endl << "# # # # # # # # #" << endl << "# # ##  ### # # #" << endl << "# # # # # # # # #" << endl << "##  # # # #  # #\033[0m" << endl << endl;
 	const char* dc[] = {"Impressive, you've achieved the impossible: a draw. I almost broke a sweat.", "A tie? Well, I suppose even a broken clock is right twice a day.", "You've found the perfect recipe for mediocrity - a draw with me!", "A draw? That's almost as rare as me making a mistake.", "I must admit, you've got some skills to pull off a draw against me.", "You've reached the pinnacle of Tic-Tac-Toe - a stalemate with the unbeatable!", "A tie game? You're like a worthy adversary... almost.", "A draw? It's almost like you're pretending to be a challenge.", "Congratulations, you've earned the title of 'Almost a Winner' in this game.", "A tie? You must be a true master of drawing games. Bravo."};
-	if(EXTRACOCKY == 1)
-		if(sw == 0)
-			cout << "\033[35mBot: \033[31m" << dc[rand () % 10] << "\033[0m" << endl << endl;
-		else
-			cout << "\033[35mBot: \033[33m" << dc[rand () % 10] << "\033[0m" << endl << endl;
+	if(EXTRACOCKY == 1){
+		if(sw == 0) cout << "\033[35mBot: \033[31m" << dc[rand () % 10] << "\033[0m" << endl << endl;
+		else cout << "\033[35mBot: \033[33m" << dc[rand () % 10] << "\033[0m" << endl << endl;
+	}
 }
 
 void lose(short compcurpos, bool sw)
 {
 	const char* wcf[] = {"Your move. Oh wait, it doesn't matter. I've already secured victory with: ", "You thought you had a shot? Adorable. The game was over when I placed: ", "You're ten moves behind, and I've just sealed the deal with: ", "You're trying so hard, but I've already won with: ", "It's almost cute how you keep playing, even though I've dominated with: ", "You see, the moment I moved, it was all over. You just didn't realize it yet.", "You're like a mariner lost at sea, and I'm the lighthouse guiding you to your inevitable loss with: ", "They say practice makes perfect, but it seems like you've got a long way to go. My move sealed the deal: ", "You're playing Tic-Tac-Toe, and I'm playing 4D chess. My winning move was: ", "You can keep trying, but the outcome is crystal clear with: "};
-	cout << endl << "\033[1;32m# #  #  # #  #    #  ### ###\033[0m" << endl << "\033[1;32m# # # # # #  #   # # #   #\033[0m" << endl << "\033[1;32m# # # # # #  #   # # ### ###\033[0m" << endl << "\033[1;32m #  # # # #  #   # #   # #\033[0m" << endl << "\033[1;32m #   #   #   ###  #  ### ###\033[0m" << endl << endl;
+	cout << endl << "\033[1;32m# #  #  # #  #    #  ### ###" << endl << "# # # # # #  #   # # #   #" << endl << "# # # # # #  #   # # ### ###" << endl << " #  # # # #  #   # #   # #" << endl << " #   #   #   ###  #  ### ###\033[0m" << endl << endl;
 	if(EXTRACOCKY == 1){
-		if(sw == 0)
-			cout << "\033[35mBot: \033[31m" << wcf[rand () % 10] << "\033[0m" << endl << endl;
-		else
-			cout << "\033[35mBot: \033[33m" << wcf[rand () % 10] << "\033[0m" << endl << endl;
+		if(sw == 0) cout << "\033[35mBot: \033[31m" << wcf[rand () % 10] << "\033[0m" << endl << endl;
+		else cout << "\033[35mBot: \033[33m" << wcf[rand () % 10] << "\033[0m" << endl << endl;
 	}
-	else
-		cout << "the losing position is: " << endl;
+	else cout << "the losing position is: " << endl;
 	display(compcurpos);
 	const char* wcf1[] = {"Well, that was cute. Almost like a real challenge!","You're playing Tic-Tac-Toe, right? I thought it was supposed to be a game, not a cakewalk.","It's like watching a master at work, and by master, I mean me.","Do you even know the rules? I mean, you're trying so hard.","You're making this game feel like a warm-up for me. Keep trying!","I hate to break it to you, but you might want to find a new hobby.","Close, but not quite. Maybe one day you'll get the hang of it.","It's almost admirable how consistently you lose.","I heard you're a Tic-Tac-Toe legend... in a parallel universe.","You're the best at losing I've ever seen. Keep up the good work!"};
-	if(sw == 0)
-		cout << "\033[35mBot: \033[31m" << wcf1[rand () % 10] << "\033[0m" << endl << endl;
-	else
-		cout << "\033[35mBot: \033[33m" << wcf1[rand () % 10] << "\033[0m" << endl << endl;
-}
-
-bool checkWin(char board[9], char sw) {
-    bitset<9> pl;
-    for (short i = 0; i < 9; ++i)
-    	pl.set(i, board[i] == sw);
-    bitset<9> win[] = {0b111000000,0b000111000,0b000000111,0b100100100,0b010010010,0b001001001,0b100010001,0b001010100};
-    for (const auto& combination : win)
-		if ((pl & combination) == combination) return true;
-    return false;
+	if(sw == 0) cout << "\033[35mBot: \033[31m" << wcf1[rand () % 10] << "\033[0m" << endl << endl;
+	else cout << "\033[35mBot: \033[33m" << wcf1[rand () % 10] << "\033[0m" << endl << endl;
 }
 
 int main(){
+    srand(time(NULL));
+    for(int i = 0; i < 19683; i++)
+        for(int u = 0; u < 9; u++)
+            pr[i][u] = 32768;
     char optipos[9] = {0};
-	int checksum = 0, temp;
-	for (short h = 0; h < 19561; ++h, ++optipos[0])
+	for (short h = 0; h < 19683; ++h, ++optipos[0])
 	{	
 		for (short i = 0; i < 8; ++i)
 			optipos[i] = (optipos[i] == 3) ? (optipos[i + 1]++, 0) : optipos[i];
-		posopti[h][9] = 0;
-		posopti[h][10] = 1;
-		for(short i = 0; i < 9; ++i)
-			if(optipos[i] == 2)
-			{
-				posopti[h][0] = i;
-				++posopti[h][10];
-				++posopti[h][9];
-				break;
-			}
-		if(posopti[h][10] == 1)
-			for(short i = 0; i < 9; ++i)
-				if (optipos[i] == 1)
-    				posopti[h][posopti[h][9]++] = i;
 		if(checkWin(optipos, 1) == 1)
 			winarr[h] = 1;
 		else if(checkWin(optipos, 2) == 1)
 			winarr[h] = 2;
 	}
-	bool fail = 0;
-	ifstream load("memory.bin", ios::binary);
-	load.read(reinterpret_cast<char*>(pr), sizeof(pr));
-	for (int i = 0; i < 18913; ++i)
-        checksum += pr[i];
-    load.read(reinterpret_cast<char*>(&temp), sizeof(int));
-	if(checksum != temp || !load)
-		fail = 1;
-	if(fail == 1){
-		load.close();
-		ofstream save("memory.bin", ios::binary | ios::trunc);
-		fill_n(pr, 18913, 0);
-		char optiarr[9] = {0};
-		for(int h = 0; h < 18913; ++h, ++optiarr[0])
-			for (short i = 0; i < 9; ++i){
-				optiarr[i] = (optiarr[i] == 3) ? (optiarr[i + 1]++, 0) : optiarr[i];
-				pr[h] += (optiarr[i] == 0) ? power3[0][i] : 0;
-			}
-		short optirand[18913], addtwo[18913] = { -1 };
-		memcpy(optirand, pr, sizeof(pr));
-		srand(2125821);
-		for (int i = 0; i < 67125; ++i)
-		{
-			short prev, last, compcurpos = 0, prevprev, lastlast;
-			for (short it = 0;; ++it)
-			{
-				if (pr[compcurpos] == 0){
-					pr[prev] -= power3[0][last];
-					break;
-				}
-				prevprev = prev;
-				prev = compcurpos;
-				last = turn(pr[compcurpos], &compcurpos, 0);
-				if (winarr[compcurpos] == 1){
-					if (posopti[pr[prev]][10] == 1)
-						pr[prev] += power3[0][last];
-					if (posopti[pr[prevprev]][10] == 1)
-						addtwo[prevprev] = lastlast;
-					break;
-				}
-				if (it == 4)
-					break;
-				turn(optirand[compcurpos], &compcurpos, 1);
-				if (winarr[compcurpos] == 2){
-					pr[prev] -= power3[0][last];
-					break;
-				}	
-				lastlast = last;
-			}
-		}
-		srand(2125821);
-		for (int i = 0; i < 71732; ++i)
-		{
-			short prev, last, compcurpos = 0, prevprev, lastlast;
-			for (short it = 0;; ++it)
-			{
-				turn(optirand[compcurpos], &compcurpos, 0);
-				if (winarr[compcurpos] == 1){
-					pr[prev] -= power3[0][last];
-					break;
-				}
-				lastlast = last;
-				if (it == 4)
-					break;
-				if (pr[compcurpos] == 0){
-					pr[prev] -= power3[0][last];
-					break;
-				}
-				prevprev = prev;
-				prev = compcurpos;
-				last = turn(pr[compcurpos], &compcurpos, 1);
-				if (winarr[compcurpos] == 2){
-					if (posopti[pr[prev]][10] == 1)
-						pr[prev] += power3[0][last];	
-					if (posopti[pr[prevprev]][10] == 1)
-						addtwo[prevprev] = lastlast;
-					break;
-				}
-			}
-		}
-		for(short i = 0; i < 18913; ++i)
-			if(addtwo[i] > -1 && (pr[i] / power3[0][addtwo[i]]) % 3 == 1 && posopti[pr[i]][10] == 1)
-				pr[i] += power3[0][addtwo[i]];
-		checksum = 0;
-		save.write(reinterpret_cast<char*>(pr), sizeof(pr));
-		for (short i = 0; i < 18913; ++i)
-			checksum += pr[i];
-		save.write(reinterpret_cast<char*>(&checksum), sizeof(int));
-		save.close();
-	}
-	system("CLS");
-	srand(time(NULL));
-	for (;;)
-	{
-		short compcurpos = 0;
-		if (rand() % 2)
-		{
-			cout << "Bot starts first" << endl;
-			for (short it = 0;; ++it){
-				turn(pr[compcurpos], &compcurpos, 0);
-				if (winarr[compcurpos] == 1){
-					lose(compcurpos, 0);
-					break;
-				}
-				display(compcurpos);
-				if (it == 4){
-					draw(0);
-					break;
-				}
-				input(&compcurpos, 1);
-			}
-		}
-		else
-		{
-			cout << "You start first" << endl;
-			for (short it = 0;; ++it)
-			{
-				display(compcurpos);
-				input(&compcurpos, 0);
-				if (it == 4){
-					draw(1);
-					break;
-				}
-				turn(pr[compcurpos], &compcurpos, 2);
-				if (winarr[compcurpos] == 2){
-					lose(compcurpos, 1);
-					break;
-				}
-			}
-		}
-	}
+    vector<short> move {0, 1, 2, 3, 4, 5, 6, 7, 8};
+    for(;;){
+        short curpos = power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]];
+        if(winarr[curpos] == 1){
+            pr[0][move[0]]++;
+            pr[power3[move[0]] + power3[move[1]] * 2][move[2]]++;
+            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2][move[4]] += 4;
+
+            pr[power3[move[0]]][move[1]]--;
+            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]]][move[3]]--;
+        }
+        else
+        {
+            curpos += power3[move[5]] * 2;
+            if(winarr[curpos] == 2){
+                pr[power3[move[0]]][move[1]]++;
+                pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]]][move[3]]++;
+                pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]]][move[5]] += 4;
+
+                pr[0][move[0]]--;
+                pr[power3[move[0]] + power3[move[1]] * 2][move[2]]--;
+                pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2][move[4]]--;
+            }
+            else
+            {
+                curpos += power3[move[6]];
+                if(winarr[curpos] == 1){
+                    pr[0][move[0]]++;
+                    pr[power3[move[0]] + power3[move[1]] * 2][move[2]]++;
+                    pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2][move[4]]++;
+                    pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]] + power3[move[5]] * 2][move[6]] += 4;
+                    
+                    pr[power3[move[0]]][move[1]]--;
+                    pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]]][move[3]]--;
+                    pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]]][move[5]]--;
+                }
+                else
+                {
+                    curpos += power3[move[7]] * 2;
+                    if(winarr[curpos] == 2){
+                        pr[power3[move[0]]][move[1]]++;
+                        pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]]][move[3]]++;
+                        pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]]][move[5]]++;
+                        pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]] + power3[move[5]] * 2 + power3[move[6]]][move[7]] += 4;
+
+                        pr[0][move[0]]--;
+                        pr[power3[move[0]] + power3[move[1]] * 2][move[2]]--;
+                        pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2][move[4]]--;
+                        pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]] + power3[move[5]] * 2][move[6]]--;
+                    }
+                    else{
+                        curpos += power3[move[8]];
+                        if(winarr[curpos] == 1){
+                            pr[0][move[0]]++;
+                            pr[power3[move[0]] + power3[move[1]] * 2][move[2]]++;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2][move[4]]++;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]] + power3[move[5]] * 2][move[6]]++;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]] + power3[move[5]] * 2 + power3[move[6]] + power3[move[7]] * 2][move[8]] += 4;
+
+                            pr[power3[move[0]]][move[1]]--;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]]][move[3]]--;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]]][move[5]]--;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]] + power3[move[5]] * 2 + power3[move[6]]][move[7]]--;
+                        }else{
+                            pr[0][move[0]]++;
+                            pr[power3[move[0]] + power3[move[1]] * 2][move[2]]++;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2][move[4]]++;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]] + power3[move[5]] * 2][move[6]]++;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]] + power3[move[5]] * 2 + power3[move[6]] + power3[move[7]] * 2][move[8]]++;
+
+                            pr[power3[move[0]]][move[1]]++;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]]][move[3]]++;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]]][move[5]]++;
+                            pr[power3[move[0]] + power3[move[1]] * 2 + power3[move[2]] + power3[move[3]] * 2 + power3[move[4]] + power3[move[5]] * 2 + power3[move[6]]][move[7]]++;
+                        }
+                    }
+                }
+            }
+        }
+        if(next_permutation(move.begin(), move.end()) == 0)
+            break;
+    }
+    pr[6724][1] += 24;
+    pr[6724][3] += 24;
+    pr[6724][5] += 24;
+    pr[6724][7] += 24;
+    pr[900][1] += 24;
+    pr[900][3] += 24;
+    pr[900][5] += 24;
+    pr[900][7] += 24;
+    pr[13210][6] += 24; // 2
+    pr[13258][2] += 24; // 4 
+    pr[2034][0] += 24; // 6
+    pr[5202][0] += 24; // 8
+    system("CLS");
+    for(;;){
+        short compcurpos = 0;
+        if(rand() % 2){
+            for(int m = 0;; m++){
+                unsigned short moveval = 0, move = 0;
+                for(int j = 0; j < 9; j++)
+                    if(pr[compcurpos][j] > moveval and (compcurpos / power3[j]) % 3 == 0){
+                        moveval = pr[compcurpos][j];
+                        move = j;
+                    }
+                compcurpos += power3[move];
+                if(winarr[compcurpos] == 1){
+                    lose(compcurpos, 0);
+                    break;
+                }
+                if(m == 4){
+                    draw(1);
+                    break;
+                }
+                display(compcurpos);
+                input(&compcurpos, 1);
+            }
+        }
+        else
+        {
+            for(int m = 0;; m++){
+                display(compcurpos);
+                input(&compcurpos, 0);
+                if(m == 4){
+                    draw(0);
+                    break;
+                }
+                unsigned short moveval = 0, move = 0;
+                for(int j = 0; j < 9; j++)
+                    if(pr[compcurpos][j] > moveval and (compcurpos / power3[j]) % 3 == 0){
+                        moveval = pr[compcurpos][j];
+                        move = j;
+                    }
+                compcurpos += 2 * power3[move];
+                if(winarr[compcurpos] == 2){
+                    lose(compcurpos, 1);
+                    break;
+                }
+            }
+        }
+    }
 }
